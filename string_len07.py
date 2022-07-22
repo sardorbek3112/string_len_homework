a961b7f9ea2fefcd6fdce75b5a04ce39df624e28
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-22 23:39:33 +0500

    string_len07.py

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,12 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a="["
+    if len(s1)%2==1:
+        a+=s1
+    if len(s2)%2==1:
+        a+=", "+s2
+    if len(s3)%2==1:
+        a+=", "+s3
+
+    return a+"]"
