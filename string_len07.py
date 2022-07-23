6190fe80f5ac8c880ccfdfd7f325c0356315eca4
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-23 08:11:10 +0500

    string_len07.py

--- string_len07.py
+++ string_len07.py
@@ -9,11 +9,21 @@ def main(s1,s2,s3):
         string
     """
     a="["
-    if len(s1)%2==1:
-        a+=s1
-    if len(s2)%2==1:
-        a+=", "+s2
-    if len(s3)%2==1:
-        a+=", "+s3
-
-    return a+"]"
+    if len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1:
+        return "["+s1+", "+s2+", "+s3+"]"
+    elif len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==0:
+        return "["+s1+", "+s2+"]"
+    elif len(s1)%2==1 and len(s2)%2==0 and len(s3)%2==1:
+        return "["+s1+", "+s3+"]"
+    elif len(s1)%2==0 and len(s2)%2==1 and len(s3)%2==1:
+        return "["+s2+", "+s3+"]"
+    elif len(s1)%2==1 and len(s2)%2==0 and len(s3)%2==0:
+        return "["+s1+"]"
+    elif len(s1)%2==0 and len(s2)%2==1 and len(s3)%2==0:
+        return "["+s2+"]"
+    elif len(s1)%2==0 and len(s2)%2==0 and len(s3)%2==1:
+        return "["+s3+"]"
+a='codeaq1'
+b="codes"
+c="coder"
+print(main(a,b,c))
